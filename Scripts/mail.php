75af312916624293dd895dea8bd8197576cbea44
Ivanandres2006 <71884124+Ivanandres2006@users.noreply.github.com>
2022-06-01 16:15:39 -0400

    Add files via upload

---
+++ Scripts/mail.php
@@ -0,0 +1,14 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+$mailheader = "From: " .$name. "<".$email. ">\r\n";
+
+$recipient = "madrizivanandres@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("ERROR!");
+
+echo"message send";
+ ?>
